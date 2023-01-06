c9b86983b651dcc020d52dff699627e530dc0976
HarshaTejaswi <77867496+HarshaTejaswi@users.noreply.github.com>
2023-01-06 22:50:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./index.css"
+import Home from "./routes/Home";
+import Contact from "./routes/Contact";
+import About from "./routes/About";
+import Project from "./routes/Project";
+
+function App() {
+  return (
+    <>
+    <Routes>
+    <Route path="http://localhost:3000/" element={<Home />} />
+    <Route path="/project" element={<Project />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
